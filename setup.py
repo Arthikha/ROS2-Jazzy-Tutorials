84d7cfa24ecb647d19260510b195197379300ab3
Arthikha <arthikha.22@cse.mrt.ac.lk>
2025-06-30 21:47:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/task3', ['core_concepts/task3/values.csv']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
